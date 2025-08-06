ecb88634841fe5d8dfaefee35bb9e3dc196061be
Denys Pavlyk <denispavlik9@gmail.com>
2025-08-06 09:52:35 -0400

    Implement debounced search by ad title

--- src/app/api/ads/route.ts
+++ src/app/api/ads/route.ts
@@ -1,10 +1,21 @@
 import { connectToDB } from "@/libs/connectToDB";
 import { AdModel } from "@/models/Ad";
 
-export async function GET() {
+export async function GET(req: Request) {
   await connectToDB();
 
-  const adsDocs = (await AdModel.find({})).reverse();
+  const { searchParams } = new URL(req.url);
+  const search = searchParams.get("search");
+
+  let adsDocs;
+
+  if (search) {
+    adsDocs = await AdModel.find({
+      title: { $regex: search, $options: "i" },
+    }).sort({ createdAt: -1 });
+  } else {
+    adsDocs = await AdModel.find({}).sort({ createdAt: -1 });
+  }
 
   return Response.json(adsDocs);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,19 @@ export default function Home() {
   return (
     <div className="flex w-full">
       <div className="w-1/4 grow p-4 border-r border-gray-200">
-        
+        <div className="relative w-full max-w-sm">
+          <label htmlFor="search" className="absolute left-3 top-1/2 -translate-y-1/2 !m-0">
+            <FontAwesomeIcon icon={faMagnifyingGlass} size="xl" />
+          </label>
+          <input
+            id="search"
+            type="text"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="bg-gray-200 w-full !pl-10 !pr-4 !py-2 placeholder:text-gray-500 outline-0"
+          />
+        </div>
       </div>
       <div className="w-3/4 grow px-8 bg-gray-100">
         <h2 className="font-bold text-xl mt-4 mb-6">Latest products</h2>
